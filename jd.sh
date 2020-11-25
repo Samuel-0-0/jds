a9df91386be66dbf9c4d6e4e1d368f4c1c9c65b0
Samuel-0-0 <imhsaw@gmail.com>
2020-11-25 09:43:53 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,44 +1,29 @@
 #!/usr/bin/bash
 
-# make our folders and links
-if [ ! -d "/config/scripts" ]; then
-  mkdir -p /config/scripts
-  echo "mkdir -p /config/scripts"
-fi
-
-if [ ! -d "/scripts" ]; then
-  mkdir /scripts
-  echo "mkdir /scripts"
-fi
-
-ln -s /config/scripts /scripts
-
-echo "User folders done"
-
 if [ -f "/scripts/index.js" ]; then
-  cd /scripts
-  git pull
-  npm install --registry=https://registry.npm.taobao.org
-  echo "pull done"
+  cd /scripts \
+  && git pull \
+  && npm install --registry=https://registry.npm.taobao.org \
+  && echo "pull done"
 else
-  git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts  && cd /scripts \
-  npm install --registry=https://registry.npm.taobao.org
-  echo "new clone done"
+  git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts \
+  && cd /scripts \
+  && npm install --registry=https://registry.npm.taobao.org \
+  && echo "new clone done"
 fi
+
+echo "git done"
 
 if [ -f "/config/${CRONTAB_LIST_FILE}" ]; then
-  crontab /config/${CRONTAB_LIST_FILE}
-  crontab -l
+  crontab /config/${CRONTAB_LIST_FILE} \
+  && crontab -l
 else
-  crontab /scripts/docker/crontab_list_ts.sh
-  crontab -l
+  crontab /scripts/docker/crontab_list_ts.sh \
+  && crontab -l
 fi
 
-crond
+crond && echo "crond done"
 
-cd /scripts
-node jd_bean_sign.js
-
-echo "签到测试完成"
+cd /scripts && node jd_bean_sign.js && echo "签到测试完成"
 
 tail -f /dev/null
