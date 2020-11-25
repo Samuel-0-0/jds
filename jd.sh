094edfe0093b388a98d79897583432271b482bde
Samuel-0-0 <imhsaw@gmail.com>
2020-11-25 09:07:00 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/bash
 
 # make our folders and links
-mkdir -p /config/scripts
-if [ ! -d "/scripts" ]; then \
+if [ ! -d "/config/scripts" ]; then
+  mkdir -p /config/scripts
+  echo "mkdir -p /config/scripts"
+fi
+
+if [ ! -d "/scripts" ]; then
   mkdir /scripts
+  echo "mkdir /scripts"
 fi
 
 ln -s /config/scripts /scripts
 
 echo "User folders done"
 
-if [ -f "/scripts/index.js" ]; then \
-  cd /scripts \
-  && git pull
+if [ -f "/scripts/index.js" ]; then
+  cd /scripts
+  git pull
+  npm install --registry=https://registry.npm.taobao.org
+  echo "pull done"
 else
-  git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts \
-  && cd /scripts \
-  && npm install
+  git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /scripts  && cd /scripts \
+  npm install --registry=https://registry.npm.taobao.org
+  echo "new clone done"
 fi
 
-echo "git done"
-
-if [ -f "/config/${CRONTAB_LIST_FILE}" ]; then \
+if [ -f "/config/${CRONTAB_LIST_FILE}" ]; then
   crontab /config/${CRONTAB_LIST_FILE}
+  crontab -l
 else
   crontab /scripts/docker/crontab_list_ts.sh
+  crontab -l
 fi
 
 crond
