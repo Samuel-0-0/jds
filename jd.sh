26561f84888c531d81cc7e02103d2022739d6c4c
Samuel-0-0 <imhsaw@gmail.com>
2020-12-01 12:27:37 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/bash
 
 npm install -g npm \
-&& echo "#################### \
-npm已更新 \
-####################"
+&& echo -e "\n#################### npm已更新 ####################\n"
 
 if [ ! -d "/scripts" ] && [ -f "/config/scripts/index.js" ]; then
   ln -s /config/scripts /scripts
   cd /scripts \
   && git pull \
-  && echo "#################### \
-  拉取git更新完成 \
-  ####################"
+  && echo -e "\n#################### 拉取git更新完成 ####################\n"
   npm cache clear --force
   npm install --registry=https://registry.npm.taobao.org
 fi
@@ -20,38 +16,34 @@ if [ ! -d "/config/scripts" ]; then
   git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /config/scripts
   cd /config/scripts
   npm install --registry=https://registry.npm.taobao.org
-  echo "#################### \
-  git克隆完成 \
-  ####################"
+  echo -e "\n#################### git克隆完成 ####################\n"
   ln -s /config/scripts /scripts \
-  && echo "###  创建软链接 /config/scripts /scripts 完成  ###"
+  && echo -e "\n#####  创建软链接 /config/scripts → /scripts 完成  #####\n"
 fi
 
 
 if [ -f "/config/${MY_CRONTAB_LIST_FILE}" ]; then
-  echo "#################### \
-  使用自定义的计划任务 \
-  ####################"
+  echo -e "\n#################### 使用自定义的计划任务 ####################\n"
   crontab /config/${MY_CRONTAB_LIST_FILE} \
   && crontab -l
 else
-  echo "#################### \
-  使用默认的计划任务 \
-  ####################"
-  crontab /scripts/docker/crontab_list_ts.sh \
+  echo -e "\n#################### 使用默认的计划任务 ####################\n"
+  crontab /scripts/docker/${CRONTAB_LIST_FILE} \
   && crontab -l
 fi
 
-crond && echo "###  开启计划任务  ###"
+crond && echo -e "\n#####  开启计划任务  #####\n"
 
 if [ ! -d "/config/logs" ]; then
   mkdir /config/logs
 fi
+
 if [ ! -d "/scripts/logs" ]; then
   ln -s /config/logs /scripts/logs \
-  && echo "###  创建软链接 /config/logs /scripts/logs 完成  ###"
+  && echo -e "\n#####  创建软链接 /config/logs → /scripts/logs 完成  #####\n"
 fi
 
-cd /scripts && node jd_bean_sign.js |ts >> /scripts/logs/jd_bean_sign.log 2>&1 && echo "###  签到测试完成  ###"
+cd /scripts && node jd_bean_sign.js |ts >> /scripts/logs/jd_bean_sign.log 2>&1 \
+&& echo -e "\n#####  签到测试完成  #####\n"
 
 tail -f /dev/null
