f6148476822040e3f07ebae656d163f082b8a93d
Samuel-0-0 <imhsaw@gmail.com>
2020-12-01 14:35:58 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -2,28 +2,32 @@
 
 echo -e "\n#################### 开始npm更新 ####################\n"
 npm install -g npm \
-&& echo -e "\n#################### npm更新完成 ####################\n"
+&& echo -e "\n#################### npm更新完成 ####################\n" \
+|| echo -e "\n#################### npm更新出错 ####################\n"
 
 if [[ ! -d "/scripts" ]] && [[ -f "/config/scripts/index.js" ]]; then
   ln -s /config/scripts /scripts
   cd /scripts
   echo -e "\n#################### 开始拉取git更新 ####################\n" \
   && git pull \
-  && echo -e "\n#################### 拉取git更新完成 ####################\n"
+  && echo -e "\n#################### 拉取git更新完成 ####################\n" \
+  || echo -e "\n#################### 拉取git更新出错 ####################\n"
   npm cache clear --force
   npm install --registry=https://registry.npm.taobao.org
 fi
 
 if [[ -d "/config/scripts" ]] && [[ ! -f "/config/scripts/index.js" ]]; then
-  rm -rf /config/scripts
+  rm -rf /config/scripts \
+  && echo -e "\n#################### 文件夹有问题删除重建 ####################\n"
 fi
 
 if [[ ! -d "/config/scripts" ]]; then
   echo -e "\n#################### 开始git克隆 ####################\n"
-  git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /config/scripts
+  git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /config/scripts \
+  && echo -e "\n#################### git克隆完成 ####################\n" \
+  || echo -e "\n#################### git克隆出错 ####################\n"
   cd /config/scripts
   npm install --registry=https://registry.npm.taobao.org
-  echo -e "\n#################### git克隆完成 ####################\n"
   ln -s /config/scripts /scripts \
   && echo -e "\n#####  创建软链接 /config/scripts → /scripts  #####\n"
 fi
