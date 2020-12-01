068089b9b69e4ddce5070054b7400f426ba5ead8
Samuel-0-0 <imhsaw@gmail.com>
2020-12-01 12:34:00 +0800

    Update jd.sh

--- jd.sh
+++ jd.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
+echo -e "\n#################### 开始npm更新 ####################\n"
 npm install -g npm \
-&& echo -e "\n#################### npm已更新 ####################\n"
+&& echo -e "\n#################### npm更新完成 ####################\n"
 
 if [ ! -d "/scripts" ] && [ -f "/config/scripts/index.js" ]; then
   ln -s /config/scripts /scripts
-  cd /scripts \
+  cd /scripts
+  echo -e "\n#################### 开始拉取git更新 ####################\n" \
   && git pull \
   && echo -e "\n#################### 拉取git更新完成 ####################\n"
   npm cache clear --force
@@ -13,12 +15,13 @@ if [ ! -d "/scripts" ] && [ -f "/config/scripts/index.js" ]; then
 fi
 
 if [ ! -d "/config/scripts" ]; then
+  echo -e "\n#################### 开始git克隆 ####################\n"
   git clone --depth=1 https://github.com/lxk0301/jd_scripts.git /config/scripts
   cd /config/scripts
   npm install --registry=https://registry.npm.taobao.org
   echo -e "\n#################### git克隆完成 ####################\n"
   ln -s /config/scripts /scripts \
-  && echo -e "\n#####  创建软链接 /config/scripts → /scripts 完成  #####\n"
+  && echo -e "\n#####  创建软链接 /config/scripts → /scripts  #####\n"
 fi
 
 
@@ -32,7 +35,7 @@ else
   && crontab -l
 fi
 
-crond && echo -e "\n#####  开启计划任务  #####\n"
+crond && echo -e "\n####################  开启计划任务  ####################\n"
 
 if [ ! -d "/config/logs" ]; then
   mkdir /config/logs
@@ -40,10 +43,11 @@ fi
 
 if [ ! -d "/scripts/logs" ]; then
   ln -s /config/logs /scripts/logs \
-  && echo -e "\n#####  创建软链接 /config/logs → /scripts/logs 完成  #####\n"
+  && echo -e "\n#####  创建软链接 /config/logs → /scripts/logs  #####\n"
 fi
 
+echo -e "\n####################  开始签到测试  ####################\n"
 cd /scripts && node jd_bean_sign.js |ts >> /scripts/logs/jd_bean_sign.log 2>&1 \
-&& echo -e "\n#####  签到测试完成  #####\n"
+&& echo -e "\n####################  签到测试完成  ####################\n"
 
 tail -f /dev/null
